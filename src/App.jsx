25677ba7b5be1169ecda1a519cbee0d34eb22cc8
RafaelReyes0816 <tj.juan.reyes.b@upds.net.bo>
2025-05-01 12:49:15 -0400

    Actualización de estructura

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ import LegalPage from './pages/LegalPage'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="services" element={<Services />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="legal" element={<LegalPage />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="legal" element={<LegalPage />} />
+        </Route>
+      </Routes>
   )
 }
